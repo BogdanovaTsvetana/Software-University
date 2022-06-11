8d3b426b774daa96087083849b1d1f4973d63470
BogdanovaTsvetana <tsvetanabb@gmail.com>
2022-06-11 15:33:24 +0100

    08.Multiplication Table

---
+++ Fundamentals/01.Basic-Syntax-Conditional-Statements-and-Loops/08. Multiplication .js
@@ -0,0 +1,9 @@
+// passed judge  100 /100
+function multiplication(arg){
+    let num = arg;
+    for(let i = 1; i <= 10; i++){
+        console.log(`${num} X ${i} = ${num * i}`);
+    }
+}
+
+multiplication(5)
