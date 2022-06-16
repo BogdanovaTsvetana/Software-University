0a0203fdd266177c9b543ab3a76ee023d705b133
BogdanovaTsvetana <tsvetanabb@gmail.com>
2022-06-16 18:04:28 +0100

    04.Convert Meters to Kilometers

---
+++ Fundamentals/02.Data types and Variables/04. Convert Meters to Kilometres.js
@@ -0,0 +1,8 @@
+// passed judge  100 / 100
+function metersToKlm(metersInput) {
+    let meters = metersInput;
+    let kilometers = meters / 1000;
+    console.log(kilometers.toFixed(2));
+}
+
+metersToKlm(1852)
