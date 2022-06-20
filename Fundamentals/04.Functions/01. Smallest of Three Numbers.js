752c427fb43a90b215d2bd6e7c00312443be79ca
BogdanovaTsvetana <tsvetanabb@gmail.com>
2022-06-20 16:38:56 +0100

    01.Smallest of Three Numbers

---
+++ Fundamentals/04.Functions/01. Smallest of Three Numbers.js
@@ -0,0 +1,32 @@
+// passed judge
+function smallestOfThree(x, y, z){
+    let smallest = Number.MAX_SAFE_INTEGER;
+
+    // if(x < smallest){
+    //     smallest = x;
+    // }
+
+    // if(y < smallest){
+    //     smallest = y;
+    // }
+
+    // if(z < smallest){
+    //     smallest = z;
+    // }
+
+    smallest = smaller(x, smallest);
+    smallest = smaller(y, smallest);
+    smallest = smaller(z, smallest);
+
+    function smaller(n, small){
+        if(n < small){
+            small = n;
+        }
+        return small;
+    }
+
+    return smallest;
+}
+
+console.log(smallestOfThree(2, 5, 3))
+
