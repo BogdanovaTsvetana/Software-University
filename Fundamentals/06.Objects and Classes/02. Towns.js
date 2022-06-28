2546dc27b071535a63b85e404eb0e4cff393b1f0
BogdanovaTsvetana <tsvetanabb@gmail.com>
2022-06-28 19:41:31 +0100

    02. Towns

---
+++ Fundamentals/06.Objects and Classes/02. Towns.js
@@ -0,0 +1,21 @@
+function towns2(input) {
+    
+    for (let line of input) {
+        let [town, latitude, longitude] = line.split(' | ');
+        
+        let obj = {
+            town,
+            latitude: Number(latitude).toFixed(2),
+            longitude: Number(longitude).toFixed(2),
+        }
+
+        console.log(obj);
+    }
+}
+
+
+
+towns2(['Sofia | 42.696552 | 23.32601',
+    'Beijing | 39.913818 | 116.363625'
+])
+
