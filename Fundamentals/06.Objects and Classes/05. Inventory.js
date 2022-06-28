6a24ccc49b274de1eb569642f0afcae477bf5a9f
BogdanovaTsvetana <tsvetanabb@gmail.com>
2022-06-28 19:47:16 +0100

    05. Inventory

---
+++ Fundamentals/06.Objects and Classes/05. Inventory.js
@@ -0,0 +1,25 @@
+function solve(data) {
+
+    data
+        .map(line => {
+            let [name, level, items] = line.split(' / ');
+            items = items.split(', ').sort((a, b) => a.localeCompare(b)).join(', ');
+            return {
+                name,
+                level: Number(level),
+                items,
+            }
+        })
+        .sort((a, b) => a.level - b.level)
+        .forEach(hero => {
+            console.log(`Hero: ${hero.name}`);
+            console.log(`level => ${hero.level}`);
+            console.log(`items => ${hero.items}`);
+        });
+}
+
+solve([
+    "Isacc / 25 / Apple, GravityGun",
+    "Derek / 12 / BarrelVest, DestructionSword",
+    "Hes / 1 / Desolator, Sentinel, Antara"
+])
