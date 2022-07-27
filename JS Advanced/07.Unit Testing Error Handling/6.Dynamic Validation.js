d29ad491edcbbdfc7216846dfd59946b4c404a2d
BogdanovaTsvetana <tsvetanabb@gmail.com>
2022-07-27 13:44:10 +0100

    6.Dynamic Validation

---
+++ JS Advanced/07.Unit Testing Error Handling/6.Dynamic Validation.js
@@ -0,0 +1,15 @@
+function validate() {
+    let emailElement = document.getElementById('email');
+    emailElement.addEventListener('change', validateEmail);
+
+    function validateEmail() {
+        let emailRegex = /^[a-z]+@[a-z]+\.[a-z]+$/;
+        let isValid = emailRegex.test(emailElement.value);
+
+        if (isValid) {
+            emailElement.classList.remove('error');
+        } else {
+            emailElement.classList.add('error');
+        }
+    }
+}
